8c945e4e56def30bcf3d4d963dd37ee607a8565f
Peter Holló <lordpejdar@gmail.com>
2019-07-16 19:29:35 +0200

    apps/simple-website Remove test hook

--- apps/simple-website/src/App.js
+++ apps/simple-website/src/App.js
@@ -1,10 +1,7 @@
 import PixelAvatar from "./components/PixelAvatar";
 import React from "react";
-import {useTest} from "@phollome/hooks"
 
 function App() {
-  const {testFunc} = useTest()
-  testFunc();
   return <PixelAvatar />;
 }
 
