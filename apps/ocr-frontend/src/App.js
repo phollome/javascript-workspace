eb461d52ff70511a42895dd0e46ef16f6f58ea76
Peter Holló <lordpejdar@gmail.com>
2019-07-17 11:08:45 +0000

    apps/ocr-frontend Use MUI for upload input

--- apps/ocr-frontend/src/App.js
+++ apps/ocr-frontend/src/App.js
@@ -1,12 +1,24 @@
 import React, { useRef } from "react";
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { useFileReader } from "@phollome/hooks";
+
+const useStyles = makeStyles(() => ({
+  input: {
+    display: "none",
+  },
+}));
 
 function App() {
   const inputRef = useRef();
   const [src, onChange] = useFileReader(inputRef);
+  const classes = useStyles();
   return (
     <>
-      <input ref={inputRef} type="file" onChange={onChange} />
+      <input id="file-input" ref={inputRef} className={classes.input} type="file" accept="image/*" onChange={onChange} />
+      <label htmlFor="file-input">
+        <Button variant="contained" color="primary" component="span">Upload</Button>
+      </label>
       <br />
       <img src={src} alt="" />
     </>
