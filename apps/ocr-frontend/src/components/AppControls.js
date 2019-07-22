483bb54847e0dc9e006013974f7277c5a066dd38
Peter Holló <lordpejdar@gmail.com>
2019-07-22 10:51:34 +0000

    apps/ocr-frontend Show dummy buttons

--- apps/ocr-frontend/src/components/AppControls.js
+++ apps/ocr-frontend/src/components/AppControls.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Button, Toolbar, Typography } from "@material-ui/core";
+import { name as AppName } from "../../package.json";
+import { makeStyles } from "@material-ui/styles";
+import { classes } from "istanbul-lib-coverage";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function AppControls(props) {
+  const classes = useStyles();
   return (
     <AppBar position="static" color="primary">
-      <Toolbar />
+      <Toolbar>
+        <Typography className={classes.title} variant="h6" color="inherit">
+          {AppName}
+        </Typography>
+        <Button color="inherit">Add API-Key</Button>
+        <Button color="inherit">Load Image</Button>
+        <Button color="inherit" disabled>Process Image</Button>
+      </Toolbar>
     </AppBar>
   );
 }
