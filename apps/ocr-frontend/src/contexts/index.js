ad55a76a1a640716276611e69b9ce2db49a5565b
Peter Holló <lordpejdar@gmail.com>
2019-07-25 14:15:17 +0000

    apps/ocr-frontend Provide ctx and comp entry

---
+++ apps/ocr-frontend/src/contexts/index.js
@@ -0,0 +1,4 @@
+import { ImageDataProvider, useImageData } from "./ImageDataContext";
+import { KeyProvider, useKey } from "./KeyContext";
+
+export { ImageDataProvider, useImageData, KeyProvider, useKey }
